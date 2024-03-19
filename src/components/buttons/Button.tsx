244a58f88ff980c5793c3619621d282bf6d67435
Tilak Thapa <iamjrtilak@gmail.com>
2024-03-19 09:46:35 +0545

    fix: default classname was replacing with provided classname from props

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -4,10 +4,14 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: 'default' | 'large'
 }
 
-const Button: React.FC<ButtonProps> = ({ variant = 'default', ...props }) => {
+const Button: React.FC<ButtonProps> = ({
+  variant = 'default',
+  className,
+  ...props
+}) => {
   return (
     <button
-      className={`text-[16px] px-6 text-white font-normal bg-background rounded-[8px] h-[48px] md:text-[18px] md:h-[54px] ${variant === 'default' ? '2xl:text-[16px] xl:h-[48px]' : '2xl:text-[20px] xl:h-[64px]'} ${props.className}`}
+      className={`text-[16px] px-6 text-white font-normal bg-background rounded-[8px] h-[48px] md:text-[18px] md:h-[54px] ${variant === 'default' ? '2xl:text-[16px] xl:h-[48px]' : '2xl:text-[20px] xl:h-[64px]'} ${className}`}
       {...props}
     >
       {props.children}
