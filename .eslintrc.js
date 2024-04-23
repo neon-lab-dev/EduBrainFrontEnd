6780f2e142d411098fe5d55876427c427f23aa5b
Tilak Thapa <iamjrtilak@gmail.com>
2024-04-23 20:04:05 +0545

    fix: types

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/prefer-optional-chain': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
   settings: {
     react: {
